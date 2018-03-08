8b60888d63574794813bd58456c98d9a625d2c28
AngelaSanz <36666078+AngelaSanz@users.noreply.github.com>
2018-03-08 11:04:41 +0100

    Update registro.php

--- registro.php
+++ registro.php
@@ -18,16 +18,23 @@
    $result = mysqli_query($con,"SELECT * FROM user WHERE email='$email'");
    $nr=mysqli_num_rows($result);
 	  if($nr=='1'){
-		  $response["success"] = 1;
-	  echo json_encode($response);}
-	  else{
-		  $response["success"] = 0;
-		  mysqli_query($con,"INSERT INTO customer (dni, phone_number, name, surname, second_surname) VALUES ('$dni',$telefono,'$nombre',
-		  '$apellido1','$apellido2')");
-		  mysqli_query($con,"INSERT INTO user (email,password,dni_user) VALUES ('$email','$pass','$dni')");
-		  
-		  echo json_encode($response);
+		  $response["success"] = 0
 	  }
+
+	  else{
+	 	 $result2 = mysqli_query($con,"SELECT * FROM customer WHERE dni='$dni'");
+		  $nr=mysqli_num_rows($result2);
+		  if($nr!='1'){
+			    mysqli_query($con,"INSERT INTO customer (dni, name, surname, second_surname) VALUES ('$dni','$nombre',
+		  '$apellido1','$apellido2')");
+			 
+		  }
+		  mysqli_query($con,"INSERT INTO user (email,password,dni_user, phone_number) VALUES ('$email','$pass','$dni', $telefono)");
+		  $response["success"] = 1;
+		 
+	  } 	
+		  echo json_encode($response);
+	  
 	
    mysqli_close($con);
 ?>
